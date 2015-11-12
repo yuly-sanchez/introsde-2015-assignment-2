08d0a811fb43e1ae017b3b24430cd3b0a3aa9751
yuly-sanchez <yuly.sanchez29@gmail.com>
2015-11-12 13:01:16 +0100

    quarto commit

--- src/introsde/lifecoach/resources/MeasureCollectionResource.java
+++ src/introsde/lifecoach/resources/MeasureCollectionResource.java
@@ -26,8 +26,7 @@ public class MeasureCollectionResource {
 	Request request;
 	
 	/**
-	 * Request #9: 
-	 * GET /measureTypes 
+	 * Request #9: GET /measureTypes 
 	 * return the list of measures type
 	 */
 	@GET
