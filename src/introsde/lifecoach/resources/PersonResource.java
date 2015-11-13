3f81a7faeff4ecd4cc64d3bf0c730f791c05e5c0
yuly-sanchez <yuly.sanchez29@gmail.com>
2015-11-13 17:46:30 +0100

    sesto commit

--- src/introsde/lifecoach/resources/PersonResource.java
+++ src/introsde/lifecoach/resources/PersonResource.java
@@ -1,7 +1,16 @@
 package introsde.lifecoach.resources;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import introsde.lifecoach.converter.DateConverter;
 import introsde.lifecoach.model.*;
+import introsde.lifecoach.wrapper.HealthMeasureHistoryListWrapper;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -10,7 +19,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
@@ -126,7 +138,132 @@ public class PersonResource {
 	public Person getPersonById(int personId) {
 		System.out.println("Reading person from DB with id: " + personId);
 		Person person = Person.getPersonById(personId);
-		System.out.println("Person: " + person.toString());
+		System.out.println(person.toString());
 		return person;
 	}
+	
+	/**
+	 * Request #6: GET/person/{id}/{measureType}
+	 * Request #11: GET/person/{id}/{measureType}?before={beforeDate}&after={afterDate}
+	 * @param idPerson
+	 * @param measureType
+	 * @return the list of values the MeasureHistory of {measureType} for person
+	 *         identified by {idPerson}
+	 * @throws Exception 
+	 */
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("{measureType}")	
+	public List<HealthMeasureHistory> getMeasureHistories(@PathParam("measureType") String measureType,
+														  @QueryParam("before") String beforeDate,
+														  @QueryParam("after") String afterDate) throws Exception {
+		
+		List<HealthMeasureHistory> measureHistories = null;
+		
+		if(beforeDate==null && afterDate==null){
+			System.out.println("--> REQUESTED: getMeasureHistories("+id+", "+measureType+")");
+			System.out.println();
+			measureHistories = HealthMeasureHistory.getMeasureHistoryByMeasureType(id, measureType);
+			return measureHistories;
+			
+		}else if(beforeDate==null && afterDate!=null){
+			System.out.println("--> REQUESTED: getMeasureHistories("+id+", "+measureType+")");
+			System.out.println();
+			measureHistories = HealthMeasureHistory.getMeasureHistoryByMeasureType(id, measureType);
+			return measureHistories;
+			
+		}else if(beforeDate!=null && afterDate==null){
+			System.out.println("--> REQUESTED: getMeasureHistories("+id+", "+measureType+")");
+			System.out.println();
+			measureHistories = HealthMeasureHistory.getMeasureHistoryByMeasureType(id, measureType);
+			return measureHistories;
+			
+		}else{
+			System.out.println("--> REQUESTED: getMeasureHistories("+id+", "+measureType+", "+beforeDate+", "+afterDate+")");
+			System.out.println();
+			
+			Calendar before = Calendar.getInstance();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			before.setTime(dateFormat.parse(beforeDate));
+			
+			Calendar after = Calendar.getInstance();
+			after.setTime(dateFormat.parse(afterDate));
+			
+			measureHistories = HealthMeasureHistory.getFilterByDatesHistory(id, measureType, before, after);
+			return measureHistories;
+		}
+
+	}
+
+	
+	/**
+	 * Request #7: GET/person/{id}/{measureType}/{mid}
+	 * @param idPerson
+	 * @param measureType
+	 * @param mid
+	 * @return the value of {measureType} identified by {mid} for person identified by {idPerson}
+	 */
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("{measureType}/{mid}")
+	public String getValueMeasureHistory(
+			@PathParam("measureType") String measureType,
+			@PathParam("mid") int mid) {
+		System.out.println("==========================================================================================================================");
+		System.out.println("\t\t\t\t\tRequest #7 : GET /person/" + id + "/" + measureType + "/" + mid);
+		System.out.println("==========================================================================================================================");
+		System.out.println();
+		HealthMeasureHistory measureHistory = null;
+		String value = null;
+		try{
+			measureHistory = HealthMeasureHistory.getMeasureHistoryByMid(id, measureType, mid);
+			value = measureHistory.getValue();
+		}catch(Exception ex){
+			System.out.println(ex.getMessage());
+			throw new RuntimeException("Get: Person with " + id + " with " + measureType + " with " + mid + " not found");
+		}
+		return value;
+	}
+
+	
+	/**
+	 * Request #10: PUT/person/{id}/{measureType}/{id}
+	 * @param idPerson
+	 * @param measureType
+	 * @param mid
+	 * @param measureHistory
+	 * @return
+	 * @throws Exception
+	 */
+	@PUT
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("{measureType}/{mid}")
+	public Response putMeasureType(
+			@PathParam("measureType") String measureType,
+			@PathParam("mid") int mid, HealthMeasureHistory measureHistory)
+			throws Exception {
+		System.out.println("==========================================================================================================================");
+		System.out.println("\t\t\t\t\tRequest #10 : PUT /person/" + id + "/" + measureType + "/" + mid);
+		System.out.println("==========================================================================================================================");
+		System.out.println();
+
+		Calendar calendar = Calendar.getInstance();
+
+		Response res;
+		HealthMeasureHistory existing = HealthMeasureHistory.getHealthMeasureHistoryById(mid);
+
+		if (existing == null) {
+			System.out.println("Update: MeasureType with " + mid + " not found");
+			res = Response.noContent().build();
+
+		} else {
+			System.out.println("Update: MeasureType with " + mid + " found");
+			res = Response.created(uriInfo.getAbsolutePath()).build();
+			existing.setValue(measureHistory.getValue());
+			existing.setTimestamp(calendar.getTime());
+			HealthMeasureHistory.updateHealthMeasureHistory(existing);
+		}
+		return res;
+	}
+	
 }
